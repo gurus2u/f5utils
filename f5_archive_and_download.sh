2d4d89395538c8227802e0c8ee650d1d6581ea57
gurus2u <47166131+gurus2u@users.noreply.github.com>
2021-02-15 13:34:00 -0800

    Update f5_archive_and_download.sh

--- f5_archive_and_download.sh
+++ f5_archive_and_download.sh
@@ -1,4 +1,4 @@
-# Guru Palnisamy
+# Guru Palanisamy
 # one liner to trigger and download backup from unix/mac terminal or windows command proxmpt using ssh client
 
 _u=$(ssh <f5_host_or_mgmt_ip> "tmsh save sys ucs \$(echo \$HOSTNAME|cut -d. -f1)-\$(date +%Y%m%d-%H%M)"|tail -1|cut -d' ' -f1) && scp <f5_host_or_mgmt_ip>:$_u ./
